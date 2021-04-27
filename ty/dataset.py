13963fa828edc27fe0158f784cdbdca83ecaa71d
taeyang916 <taeyang916@github.com>
2021-04-28 00:05:56 +0900

    add dataloder

--- ty/dataset.py
+++ ty/dataset.py
@@ -1,3 +1,4 @@
+#%%
 from __future__ import print_function, division
 import os
 import torch
@@ -7,44 +8,38 @@ import numpy as np
 import glob as glob
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
+from PIL import Image
 from torchvision import transforms, utils
+import torchvision
 
-root_path = "/fruits-360"
-Training_path = "/Training"
-Test_path = "/Test"
-
-"""
-workspace_path = os.path.dirname(os.path.realpath(__file__))
-dir_path = workspace_path + root_path + Training_path
-
-os.listdir(dir_path)
-
-print("before: %s"%os.getcwd())
-os.chdir("./ty" + root_path + Training_path)
-print("after: %s"%os.getcwd())
-
-test_dir = os.listdir(os.getcwd())
-#test_dir.sort()
-print(test_dir)
-
-for i in test_dir:
-    print(i)
-    test_dir += 'i'
-"""
-
-data = [{'dataname':[], 'idx':[]}]
 class FruitDataLoader(DataLoader):
-    def __init__(self, root='./fruits-360', transform=False):
-        super.__init__(FruitDataLoader)
+    def __init__(self, dataset_type='Training', transform=None):
+        self.dataset = pd.read_csv(f'./{dataset_type}.csv')
+        self.image_path = self.dataset['image_path']
+        self.image_idx = self.dataset['image_idx']
+        self.transform = transform
 
-
-
-        datasets_list = os.listdir(os.getcwd()).sort()
 
     def __getitem__(self, idx):
-        
-        return
+        x = Image.open(self.image_path[idx])
+        y = self.image_idx[idx]
+        if self.transform:
+            x = self.transform(x)
+        return x, y
     
     def __len__(self):
+        return len(self.dataset)
 
-        return
+"""
+transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.25, 0.25, 0.25)),
+])
+
+# dataset_type : 'Training' or 'Test'
+train_dataset = FruitDataLoader(dataset_type='Training', transform=transform)
+trainloader = DataLoader(train_dataset, batch_size=10, shuffle=True)
+
+test_dataset = FruitDataLoader(dataset_type='Test', transform=transform)
+testloader = DataLoader(test_dataset, batch_size=10, shuffle=False)
+"""
